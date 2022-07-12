d04fa6e01a989d688373533fb4548701ae4ff783
Nathan <nmcaleese87@gmail.com>
2022-07-12 11:25:54 -0700

    adds main to apps fixes template and migration bug

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Shirt(models.Model):
+    band = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    size = models.CharField(max_length=5)
+    color = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.band}'s {self.description} shirt"
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-class Shirt:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, band, description, size, color):
-    self.band = band
-    self.description = description
-    self.size = size
-    self.color = color
-
-shirts = [
-  Shirt('ACDC', 'Back in Black Tour', 'L', 'Black'),
-  Shirt('Avril Lavigne', 'Sk8r Boi Tour', 'S', 'Red'),
-  Shirt('Bob Dylan', 'Smoking', 'M', 'Grey')
-]
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello</h1>")
+  return render(request, "home.html")
 
 
 def about(request):
