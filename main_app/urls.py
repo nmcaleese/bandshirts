1c946fa1970703e508331b173beda61dda38792d
Nathan <nmcaleese87@gmail.com>
2022-07-12 07:09:11 -0700

    completes exercises in lab 1, but cannot find template folder to debug

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    # route for bandshirts index
+    path("shirts/", views.shirts_index, name="index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Shirt:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, band, description, size, color):
+    self.band = band
+    self.description = description
+    self.size = size
+    self.color = color
+
+shirts = [
+  Shirt('ACDC', 'Back in Black Tour', 'L', 'Black'),
+  Shirt('Avril Lavigne', 'Sk8r Boi Tour', 'S', 'Red'),
+  Shirt('Bob Dylan', 'Smoking', 'M', 'Grey')
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Hello</h1>")
@@ -8,3 +21,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def shirts_index(request):
+    return render(request, 'shirts/index.html', {'shirts': shirts })
