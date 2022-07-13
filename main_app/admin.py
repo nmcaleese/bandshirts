2b7687cab4e4626bf93403ea311f066fe1f708c6
Nathan <nmcaleese87@gmail.com>
2022-07-13 08:08:08 -0700

    adds shirt model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Band
+from .models import Band, Shirt
 
 # Register your models here.
 admin.site.register(Band)
+admin.site.register(Shirt)
--- main_app/models.py
+++ main_app/models.py
@@ -18,15 +18,15 @@ class Band(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'band_id': self.id})
 
-# class Design(models.Model):
-#     title = models.CharField(max_length=10)
-#     color = models.CharField(max_length=10)
-#     size = models.CharField(
-#         max_length=1,
-#         choices=SIZES,
-#         default=SIZES[1][0]
-#         )
-#     band = models.ForeignKey(Band, on_delete=models.CASCADE)
+class Shirt(models.Model):
+    design = models.CharField(max_length=100)
+    color = models.CharField(max_length=10)
+    size = models.CharField(
+        max_length=1,
+        choices=SIZES,
+        default=SIZES[1][0]
+        )
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"{self.title}, in {self.color}, size:{self.get_size_display()}"
+    def __str__(self):
+        return f"{self.design}, in {self.color}, size:{self.get_size_display()}"
